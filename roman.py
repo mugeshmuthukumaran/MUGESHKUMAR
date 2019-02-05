c3204ca556871f00bf46ddee374765ca47098179
mugeshmuthukumaran <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-02-05 23:30:14 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,41 @@
+r=(raw_input())
+if (r == 'I'):
+    print "1"
+if (r == 'II'):
+    print "2"
+if (r == 'III'):
+    print "3"
+if (r == 'IV'):
+    print "4"
+if (r == 'V'):
+    print "5"
+if (r == 'VI'):
+    print "6"
+if (r == 'VII'):
+    print "7"
+if (r == 'VIII'):
+    print "8"
+if (r == 'IX'):
+   print "9"
+if (r == 'X'):
+    print "10"
+if (r == 'XI'):
+    print"11"
+if (r == 'XII'):
+    print "12"
+if (r == 'XIII'):
+    print "13"
+if (r == 'XIV'):
+    print "14"
+if (r == 'XV'):
+    print"15"
+if (r == 'XVI'):
+    print"16"
+if (r == 'XVII'):
+    print "17"
+if (r == 'XVIII'):
+    print"18"
+if (r == 'XIX'):
+    print "19"
+if (r == 'XX'):
+    print "20"
