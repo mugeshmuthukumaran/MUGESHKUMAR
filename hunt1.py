fede19b80e87e988c833d16d20c00c11d1783490
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-06-08 18:07:08 +0530

    Create hunt1.py

---
+++ hunt1.py
@@ -0,0 +1,12 @@
+#mugi
+x=int(input())
+y=list(map(int,input().split()))
+z=[]
+for i in y:
+    if(y.count(i)>1):
+        z.append(i)
+E=set(z)
+if len(E)==0:
+    print("unique")
+else:
+    print(*E)
