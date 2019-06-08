09bff6fd8db2151094ede854107feff22f2c93ca
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-06-08 19:28:00 +0530

    Update pro13.py

--- pro13.py
+++ pro13.py
@@ -1,7 +1,13 @@
 #mugi
-num,k=map(int,input().split())
-lis=list(map(int,input().split()))
-for i in range(k):
-    u,v=map(int,input().split())
-    c=lis[u-1:v]
-    print(min(c))
+n,k=map(int,input().split())
+m=list(map(int,input().split()))
+q=[]
+for i in range(0,k):
+    d = []
+    d=list(map(int,input().split()))
+    s = d[0]
+    for j in range(min(d)-1,max(d)):
+        if s>m[j]: s=m[j]
+    q.append(s)
+for i in range(0,len(q)):
+    print(q[i])
