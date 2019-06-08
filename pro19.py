1da9c6f00fa9975f920b582160f3d12eba33c7c9
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-06-08 19:53:58 +0530

    Update pro19.py

--- pro19.py
+++ pro19.py
@@ -1,11 +1,9 @@
 #mugi
-a=int(input())
-b=[]
-c=[]
-for i in range(0,a):
-    b=[int(x) for x in input().split()]
-    for j in range(0,len(b)):
-        c.append(m2[j])
-    b=[]    
-c.sort()
-print(*c)
+t=int(input())
+l1=[]
+for i in range(t):
+	l0=list(map(int,input().split()))
+	for i in l0:
+		l1.append(i)
+l1.sort()
+print(*l1)
