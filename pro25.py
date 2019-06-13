715ef54e0292bd7ddf05c39b3881b3b95021df22
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-06-13 14:00:41 +0530

    Create pro25.py

---
+++ pro25.py
@@ -0,0 +1,14 @@
+
+#mugi
+n1=int(input())
+l1=list(map(int,input().split()))
+m1=[]
+a1=1
+for i in range(n1-1):
+    if l1[i]<l1[i+1]:
+        a1+=1
+    else:
+        m1.append(a1)
+        a1=1
+m1.append(a1)
+print(max(m1))
