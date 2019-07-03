99ea7ec143b64289fe021c9cf8cf4fb1a6ee612e
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-07-03 17:59:11 +0530

    Create pro42.py

---
+++ pro42.py
@@ -0,0 +1,10 @@
+#mugi
+vk,vj=map(int,input().split())
+lis=list(map(int,input().split()))
+if vj==1:
+    print(min(lis))
+elif vj==2:
+    print(max(lis[0],lis[vk-1]))
+else:
+    print(max(lis))
+© 2019 GitHub, Inc.
