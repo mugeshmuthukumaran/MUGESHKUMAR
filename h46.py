8bede48e14df362c5647e58b19dce7a45ed03e8c
MUGESHKUMAR M <46455468+mugeshmuthukumaran@users.noreply.github.com>
2019-07-03 21:53:36 +0530

    Create h46.py

---
+++ h46.py
@@ -0,0 +1,19 @@
+#mugi
+n = int(input())
+play = list(map(int,input().split()))
+j = 1
+miner = sum(play)
+ind = 0
+while(j<n):
+    x = sum(play[:j])
+    y = sum(play[j:])
+    if(x>y):
+        res=x-y
+    else:
+        res=y-x
+    if(res<miner):
+       miner = res
+       ind = n-j
+       t1 = j
+    j+=1
+print(t1-ind)
